e48240f610a777d814430eb94adf0b5af1e6f2d5
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2024-04-05 11:48:20 -0400

    prep for submission: behavior trial analyses

---
+++ live-male-behavior-trials.R
@@ -0,0 +1,132 @@
+# This script contains the R code to work with behavior data from Carla Gutierrez
+#
+# Authors: Aguillon SM, et al.
+# Year: 2024
+# Title: Pervasive gene flow despite strong and varied reproductive barriers in swordtails
+#        
+# Journal Info: TBD
+# bioRxiv DOI: TBD
+#
+# Edited date: 18 Dec 2023
+#
+# Please cite the paper if you use these scripts
+#
+
+
+# load packages
+library(tidyverse)
+library(cowplot)
+library(readxl)
+
+# plot colors
+corcol <- "#359D6F"
+bircol <- "#3A6BC6"
+
+
+
+# load datafile
+df <- read_excel("./data/live-male-behavior-data.xlsx", sheet=1, col_names=TRUE, na="NA", trim_ws=TRUE)
+
+# trial length
+trial_time <- 600*2
+
+# remove samples where males were marked as having FGS after trials
+# remove samples from Pumphouse, which is now known to be a hybrid population
+df_keep <- df %>%
+  filter(orig_removed=="N")
+
+# test for side bias and remove any fish that spend >80% of their time on one side
+biased_df <- df_keep %>%
+  group_by(new_ID) %>%
+  summarize(pair1_bias1 = (Xbir_t1+Xcor_t2)/trial_time,
+            pair1_bias2 = (Xcor_t1+Xbir_t2)/trial_time,
+            pair2_bias1 = (Xbir_t3+Xcor_t4)/trial_time, 
+            pair2_bias2 = (Xcor_t3+Xbir_t4)/trial_time,
+            pair1_side_biased = ifelse(max(pair1_bias1, pair1_bias2)>0.800, "biased", "unbiased"),
+            pair2_side_biased = ifelse(max(pair2_bias1, pair2_bias2)>0.800, "biased", "unbiased"))
+
+joined_df <- left_join(df_keep, biased_df, by="new_ID")
+
+
+# separate the two pairs of trials from each other, remove samples with side bias
+pair1_trials <- select(joined_df, new_ID:male_pair_ID, Xbir_t1:Xcor_t2, trial_type, pair1_bias1:pair1_bias2, pair1_side_biased) %>%
+  filter(pair1_side_biased=="unbiased")
+pair2_trials <- select(joined_df, new_ID:male_pair_ID, Xbir_t3:Xcor_t4, trial_type, pair2_bias1:pair2_bias2, pair2_side_biased) %>%
+  filter(pair2_side_biased=="unbiased")
+
+# calculate association times
+pair1_trials <- pair1_trials %>%
+  mutate(Xcor_pref = Xcor_t1 + Xcor_t2 - Xbir_t1 - Xbir_t2,
+         SOP = Xcor_pref/(Xcor_t1 + Xcor_t2 + Xbir_t1 + Xbir_t2),
+         trial_num = "first") %>%
+  select(new_ID, species, trial_type, Xcor_pref, trial_num, SOP)
+
+pair2_trials <- pair2_trials %>%
+  mutate(Xcor_pref = Xcor_t3 + Xcor_t4 - Xbir_t3 - Xbir_t4,
+         SOP = Xcor_pref/(Xcor_t3 + Xcor_t4 + Xbir_t3 + Xbir_t4),
+         trial_num = "second") %>%
+  select(new_ID, species, trial_type, Xcor_pref, trial_num, SOP)
+
+full_assoc <- bind_rows(pair1_trials, pair2_trials) %>%
+  mutate(trial_type=ifelse(trial_type=="vis+odor","visual+olfactory","visual"))
+
+
+
+
+
+
+##### behavior trial results for in-text figure (Figure 2B) #####
+ggplot(data=full_assoc, aes(x=species, y=SOP)) +
+  facet_grid(~factor(trial_type, levels=c("visual","visual+olfactory"))) +
+  geom_hline(yintercept=0,color="dark gray") + 
+  geom_boxplot(aes(fill=species), outlier.shape=NA) +
+  geom_jitter(color="gray", width=0.1) +
+  scale_fill_manual(values=c(bircol, corcol)) + 
+  theme_bw() +
+  xlab("Species") +
+  ylab("Relative preference for cortezi") +
+  ylim(c(-1.0,1.1)) + 
+  scale_x_discrete(labels=c("birchmanni", "cortezi")) +
+  theme(legend.position="none", axis.title=element_text(face="bold",size=10), axis.text=element_text(size=8,color="black"),strip.text.x=element_text(size=10,face="bold"))
+# export as 3.5" x 3" PDF
+# in Illustrator: italicized species names, fixed plot titles, added significance asterisks
+# March 2024 note: ended up combining this with figure on mother/embryo sequencing!
+
+
+
+
+
+# statistically test results for SOP (Strength of Preference)
+## visual trials
+wilcox.test(filter(full_assoc, species=="birchmanni" & trial_type=="visual")$SOP, mu=0, alternative="less")
+#Wilcoxon signed rank exact test
+#
+#data:  filter(full_assoc, species == "birchmanni" & trial_type == "visual")$SOP
+#V = 416, p-value = 0.4288
+#alternative hypothesis: true location is less than 0
+wilcox.test(filter(full_assoc, species=="cortezi" & trial_type=="visual")$SOP, mu=0, alternative="great")
+#Wilcoxon signed rank test with continuity correction
+#
+#data:  filter(full_assoc, species == "cortezi" & trial_type == "visual")$SOP
+#V = 144, p-value = 0.005765
+#alternative hypothesis: true location is greater than 0
+
+## visual + olfactory trials
+wilcox.test(filter(full_assoc, species=="birchmanni" & trial_type=="visual+olfactory")$SOP, mu=0, alternative="less")
+#Wilcoxon signed rank test with continuity correction
+#
+#data:  filter(full_assoc, species == "birchmanni" & trial_type == "visual+olfactory")$SOP
+#V = 347.5, p-value = 0.5932
+#alternative hypothesis: true location is less than 0
+wilcox.test(filter(full_assoc, species=="cortezi" & trial_type=="visual+olfactory")$SOP, mu=0, alternative="great")
+#Wilcoxon signed rank test with continuity correction
+#
+#data:  filter(full_assoc, species == "cortezi" & trial_type == "visual+olfactory")$SOP
+#V = 196, p-value = 0.6829
+#alternative hypothesis: true location is greater than 0
+
+
+
+
+
+
---
+++ olfactory-behavior-trials.R
@@ -0,0 +1,189 @@
+# This script contains the R code to analyze EthoVision behavior results
+#
+# Authors: Aguillon SM, et al.
+# Year: 2024
+# Title: Pervasive gene flow despite strong and varied reproductive barriers in swordtails
+#        
+# Journal Info: TBD
+# bioRxiv DOI: TBD
+#
+# Edited date: 22 Jan 2024
+#
+# Please cite the paper if you use these scripts
+
+# load packages
+library(tidyverse)
+library(cowplot)
+library(ggrepel)
+
+# plot colors
+corcol <- "#359D6F"
+bircol <- "#3A6BC6"
+
+
+### load data ###
+
+# olfactory trials using allopatric females
+allopatric_summary <- read_tsv("./pheromone-filtered-trial-summary.txt", col_names=TRUE)
+allopatric_preference <- read_tsv("./pheromone-trial-preference.txt", col_names=TRUE) %>%
+  mutate(prop_neutral = 1 - prop_either)
+
+# olfactory trials using females from Chapulhuacanito
+CHPL_summary <- read_tsv("./CHPL-pheromone-filtered-trial-summary.txt", col_names=TRUE)
+CHPL_preference <- read_tsv("./CHPL-pheromone-trial-preference.txt", col_names=TRUE) 
+# females of unknown ancestry, and later sequenced
+CHPL_ancestry <- read_tsv("./data/CHPL-contemporary-ancestry_behaviortrial-subset.txt", col_names=TRUE)
+
+CHPL_preference_2 <- left_join(CHPL_preference, CHPL_ancestry, by="fish_ID") %>%
+  mutate(ancestry_cluster = ifelse(hybrid_index < 0.10, "birchmanni", "cortezi-like"),
+         prop_neutral = 1 - prop_either)
+
+
+
+##### behavior trial results: allopatric samples, SOP #####
+allopatric_SOP <- ggplot(data=allopatric_preference, aes(x=species, y=SOP)) +
+  geom_hline(yintercept=0,color="dark gray") + 
+  geom_boxplot(aes(fill=species), outlier.shape=NA) +
+  geom_jitter(color="gray", width=0.1) +
+  scale_fill_manual(values=c(bircol, corcol)) + 
+  theme_bw() +
+  xlab("Species") +
+  ylab("Relative preference for cortezi") +
+  ylim(c(-1.0,1.1)) + 
+  scale_x_discrete(labels=c("birchmanni", "cortezi")) +
+  theme(legend.position="none", axis.title=element_text(face="bold",size=12), axis.text=element_text(size=10,color="black"),strip.text.x=element_text(size=12,face="bold"))
+
+# statistically test results for SOP (Strength of Preference)
+## allopatric trials
+wilcox.test(filter(allopatric_preference, species=="birchmanni")$SOP, mu=0, alternative="less")
+#Wilcoxon signed rank test with continuity correction
+#
+#data:  filter(allopatric_preference, species == "birchmanni")$SOP
+#V = 83, p-value = 0.7886
+#alternative hypothesis: true location is less than 0
+wilcox.test(filter(allopatric_preference, species=="cortezi")$SOP, mu=0, alternative="great")
+#data:  filter(allopatric_preference, species == "cortezi")$SOP
+#V = 94, p-value = 0.2105
+#alternative hypothesis: true location is greater than 0
+
+
+##### behavior trial results: allopatric samples, neutral zone #####
+allopatric_neutral <- ggplot(data=allopatric_preference, aes(x=species, y=prop_neutral)) +
+  geom_hline(yintercept=1/3,color="dark gray") + 
+  geom_boxplot(aes(fill=species), outlier.shape=NA) +
+  geom_jitter(color="gray", width=0.1) +
+  scale_fill_manual(values=c(bircol, corcol)) + 
+  theme_bw() +
+  xlab("Species") +
+  ylab("Proportion time in neutral zone") +
+  ylim(c(-0.05,1.05)) + 
+  scale_x_discrete(labels=c("birchmanni", "cortezi")) +
+  theme(legend.position="none", axis.title=element_text(face="bold",size=12), axis.text=element_text(size=10,color="black"),strip.text.x=element_text(size=12,face="bold"))
+
+# statistically test results for neutral zone
+## allopatric trials
+wilcox.test(filter(allopatric_preference, species=="birchmanni")$prop_neutral, mu=1/3)
+#Wilcoxon signed rank exact test
+#
+#data:  filter(allopatric_preference, species == "birchmanni")$prop_neutral
+#V = 99, p-value = 0.1167
+#alternative hypothesis: true location is not equal to 0.3333333
+wilcox.test(filter(allopatric_preference, species=="cortezi")$prop_neutral, mu=1/3)
+#Wilcoxon signed rank exact test
+#
+#data:  filter(allopatric_preference, species == "cortezi")$prop_neutral
+#V = 14, p-value = 0.001678
+#alternative hypothesis: true location is not equal to 0.3333333
+
+wilcox.test(filter(allopatric_preference, species=="birchmanni")$prop_neutral, filter(allopatric_preference, species=="cortezi")$prop_neutral)
+#Wilcoxon rank sum exact test
+#
+#data:  filter(allopatric_preference, species == "birchmanni")$prop_neutral and filter(allopatric_preference, species == "cortezi")$prop_neutral
+#W = 221, p-value = 0.001634
+#alternative hypothesis: true location shift is not equal to 0
+
+
+
+
+
+
+##### behavior trial results: CHPL samples #####
+CHPL_SOP <- ggplot(data=CHPL_preference_2, aes(x=ancestry_cluster, y=SOP)) +
+  geom_hline(yintercept=0,color="dark gray") + 
+  geom_boxplot(aes(fill=ancestry_cluster), outlier.shape=NA) +
+  geom_jitter(color="gray", width=0.1) +
+  scale_fill_manual(values=c(bircol, corcol)) + 
+  theme_bw() +
+  xlab("Ancestry cluster") +
+  ylab("Relative preference for cortezi") +
+  ylim(c(-1.0,1.1)) + 
+  scale_x_discrete(labels=c("birchmanni", "cortezi-like")) +
+  theme(axis.title.y=element_blank(), legend.position="none", axis.title=element_text(face="bold",size=12), axis.text=element_text(size=10,color="black"),strip.text.x=element_text(size=12,face="bold"))
+
+# statistically test results for SOP (Strength of Preference)
+## allopatric trials
+wilcox.test(filter(CHPL_preference_2, ancestry_cluster=="birchmanni")$SOP, mu=0, alternative="less")
+#Wilcoxon signed rank exact test
+#
+#data:  filter(CHPL_preference_2, ancestry_cluster == "birchmanni")$SOP
+#V = 60, p-value = 0.511
+#alternative hypothesis: true location is less than 0
+wilcox.test(filter(CHPL_preference_2, ancestry_cluster=="cortezi-like")$SOP, mu=0, alternative="great")
+#Wilcoxon signed rank exact test
+#
+#data:  filter(CHPL_preference_2, ancestry_cluster == "cortezi-like")$SOP
+#V = 49, p-value = 0.5961
+#alternative hypothesis: true location is greater than 0
+
+
+##### behavior trial results: CHPL samples, neutral zone #####
+CHPL_neutral <- ggplot(data=CHPL_preference_2, aes(x=ancestry_cluster, y=prop_neutral)) +
+  geom_hline(yintercept=1/3,color="dark gray") + 
+  geom_boxplot(aes(fill=ancestry_cluster), outlier.shape=NA) +
+  geom_jitter(color="gray", width=0.1) +
+  scale_fill_manual(values=c(bircol, corcol)) + 
+  theme_bw() +
+  xlab("Ancestry cluster") +
+  ylab("Proportion time in neutral zone") +
+  ylim(c(-0.05,1.05)) + 
+  scale_x_discrete(labels=c("birchmanni", "cortezi-like")) +
+  theme(axis.title.y=element_blank(), legend.position="none", axis.title=element_text(face="bold",size=12), axis.text=element_text(size=10,color="black"),strip.text.x=element_text(size=12,face="bold"))
+
+# statistically test results for neutral zone
+## allopatric trials
+wilcox.test(filter(CHPL_preference_2, ancestry_cluster=="birchmanni")$prop_neutral, mu=1/3)
+#Wilcoxon signed rank exact test
+#
+#data:  filter(CHPL_preference_2, ancestry_cluster == "birchmanni")$prop_neutral
+#V = 79, p-value = 0.3028
+#alternative hypothesis: true location is not equal to 0.3333333
+wilcox.test(filter(CHPL_preference_2, ancestry_cluster=="cortezi-like")$prop_neutral, mu=1/3)
+#Wilcoxon signed rank exact test
+#
+#data:  filter(CHPL_preference_2, ancestry_cluster == "cortezi-like")$prop_neutral
+#V = 0, p-value = 0.0001221
+#alternative hypothesis: true location is not equal to 0.3333333
+
+wilcox.test(filter(CHPL_preference_2, ancestry_cluster=="birchmanni")$prop_neutral, filter(CHPL_preference_2, ancestry_cluster=="cortezi-like")$prop_neutral)
+#Wilcoxon rank sum exact test
+#
+#data:  filter(CHPL_preference_2, ancestry_cluster == "birchmanni")$prop_neutral and filter(CHPL_preference_2, ancestry_cluster == "cortezi-like")$prop_neutral
+#W = 197, p-value = 9.619e-06
+#alternative hypothesis: true location shift is not equal to 0
+
+
+
+
+
+#### plot the combined results for the supplement, SOP ####
+plot_grid(allopatric_SOP, CHPL_SOP, align="h", nrow=1)
+# export as 6.5" x 4" PDF
+# in Illustrator: italicized species names, fixed plot titles, added panel letters
+
+
+#### plot the combined results for the supplement, neutral zone ####
+plot_grid(allopatric_neutral, CHPL_neutral, align="h", nrow=1)
+# export as 6.5" x 4" PDF
+# in Illustrator: italicized species names, fixed plot titles, added panel letters
+
+
