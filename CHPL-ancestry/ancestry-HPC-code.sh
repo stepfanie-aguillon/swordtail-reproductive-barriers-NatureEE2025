e5067b145be017b57da595234c3a365f76148fff
Stepfanie M Aguillon <38443540+stepfanie-aguillon@users.noreply.github.com>
2025-05-14 15:23:22 -0700

    Update ancestry-HPC-code.sh

--- CHPL-ancestry/ancestry-HPC-code.sh
+++ CHPL-ancestry/ancestry-HPC-code.sh
@@ -1,11 +1,11 @@
 # This includes scripts used on the HPC for ancestry analyses
 #
 # Authors: Aguillon SM, et al.
-# Year: 2024
+# Year: 2025
 # Title: Pervasive gene flow despite strong and varied reproductive barriers in swordtails
 #        
-# Journal Info: TBD
-# bioRxiv DOI: https://doi.org/10.1101/2024.04.16.589374
+# Journal Info: Nature Ecology and Evolution
+# DOI: https://doi.org/10.1038/s41559-025-02669-9
 #
 # Edited date: 14 Nov 2023
 #
